1d543dc52561bb80aa109cafcbc15b3e802a62d7
BertoPolo <bertopolo91@gmail.com>
2022-04-14 11:26:50 +0200

    a

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express from "express"
+
+const server = express()
+
+const port = process.env.PORT || 3003
+
+const moviesRouter = express.Router()
+
+moviesRouter.use("/movies")
+server.use(express.json())
+
+server.listen(port, () => {
+  console.log("server is running on port nº : ", port)
+})
